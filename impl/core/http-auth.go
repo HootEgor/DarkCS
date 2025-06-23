3d7676e4b9ab01796abb6d3663820a600366db7e
egorh <egor.hoot@gmail.com>
2025-06-23 15:16:51 +0200

    remove commented-out authentication logic in http-auth.go

--- impl/core/http-auth.go
+++ impl/core/http-auth.go
@@ -14,19 +14,19 @@ func (c *Core) AuthenticateByToken(token string) (*entity.UserAuth, error) {
 		return &entity.UserAuth{Username: userName}, nil
 	}
 
-	userName, err := c.repo.CheckApiKey(token)
-	if err == nil {
-		c.log.With("username", userName).Debug("user authenticated from database")
-		c.keys[token] = userName
-		return &entity.UserAuth{Username: userName}, nil
-	}
-
-	if c.authKey == token {
-		userName = "internal"
-		c.log.With("username", userName).Debug("user authenticated from config")
-		c.keys[token] = userName
-		return &entity.UserAuth{Username: userName}, nil
-	}
+	//userName, err := c.repo.CheckApiKey(token)
+	//if err == nil {
+	//	c.log.With("username", userName).Debug("user authenticated from database")
+	//	c.keys[token] = userName
+	//	return &entity.UserAuth{Username: userName}, nil
+	//}
+	//
+	//if c.authKey == token {
+	//	userName = "internal"
+	//	c.log.With("username", userName).Debug("user authenticated from config")
+	//	c.keys[token] = userName
+	//	return &entity.UserAuth{Username: userName}, nil
+	//}
 
 	return nil, fmt.Errorf("invalid token")
 }
