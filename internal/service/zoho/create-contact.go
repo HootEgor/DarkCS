18c0b9f50063c8003efb3e8406fc3d421511a116
egorh <egor.hoot@gmail.com>
2025-07-28 10:47:03 +0200

    fix: refresh Zoho token if expired before creating contact

--- internal/service/zoho/create-contact.go
+++ internal/service/zoho/create-contact.go
@@ -2,9 +2,16 @@ package services
 
 import (
 	"DarkCS/entity"
+	"time"
 )
 
 func (s *ZohoService) CreateContact(user *entity.User) (string, error) {
+	if time.Now().After(s.tokenExpiresIn.Add(time.Minute * time.Duration(-5))) {
+		err := s.refreshTokenCall()
+		if err != nil {
+			return "", err
+		}
+	}
 
 	contact := user.ToContact()
 
