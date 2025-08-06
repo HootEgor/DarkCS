dfc6adf6a8899469794623758388c32905deeef8
egorh <egor.hoot@gmail.com>
2025-08-06 13:12:26 +0200

    add: doc comment for Message struct in entity definition

--- entity/message.go
+++ entity/message.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// Message User represents a user in the system.
 type Message struct {
 	User     *User     `json:"user" bson:"user"`
 	Question string    `json:"question" bson:"question"`
