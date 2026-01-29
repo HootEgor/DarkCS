ef99c45c34f364a7be632683a80b1def8b993dae
egorh <egor.hoot@gmail.com>
2026-01-29 13:56:16 +0100

    fix: update TTN message formatting with clickable tracking link
    - Replaced plaintext TTN tracking URL with a proper HTML anchor tag for enhanced user experience.

--- bot/workflows/mainmenu/steps.go
+++ bot/workflows/mainmenu/steps.go
@@ -549,7 +549,7 @@ func formatOrderMessage(order *entity.OrderDetail, customerName string) string {
 	}
 
 	if order.TTN != "" {
-		msg += fmt.Sprintf("\n<b>ТТН:</b> %s (https://novaposhta.ua/tracking/%s)", order.TTN, order.TTN)
+		msg += fmt.Sprintf("\n<b>ТТН:</b> <a href=\"https://novaposhta.ua/tracking/%s\">%s</a>", order.TTN, order.TTN)
 	}
 
 	return msg
@@ -564,7 +564,7 @@ func formatOrderMessageNumbered(order *entity.OrderDetail, customerName string, 
 	}
 
 	if order.TTN != "" {
-		msg += fmt.Sprintf("\n<b>ТТН:</b> %s (https://novaposhta.ua/tracking/%s)", order.TTN, order.TTN)
+		msg += fmt.Sprintf("\n<b>ТТН:</b> <a href=\"https://novaposhta.ua/tracking/%s\">%s</a>", order.TTN, order.TTN)
 	}
 
 	return msg
